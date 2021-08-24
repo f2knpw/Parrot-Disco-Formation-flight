72a4e4daa1f9f8923f2f812ceb1fc2c9d5d97d89
freedom2000 <31324055+f2knpw@users.noreply.github.com>
2021-08-24 21:18:44 +0200

    Update spysc2.sh

--- disco4g-parachute_1.2_mod_f2knpw/skycontroller2/uavpal/bin/spysc2.sh
+++ disco4g-parachute_1.2_mod_f2knpw/skycontroller2/uavpal/bin/spysc2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ip="192.168.42.25"
+ipESP="192.168.42.25"
 set="/dev/input/event0"
 IFS=$' \n'
 
@@ -8,7 +8,7 @@ while true; do
 	evtest ${set} | while read -r f1 f2 f3 f4 f5 f6 f7 cod f9 f10 val; do
         if [ $f7="code" ]; then
             case $cod in
-            0|1|2|3|292|293|294) echo -e "g$cod $val" | ./data/lib/ftp/uavpal/bin/netcat-arm -u ${ip} 8888 -w1&;;
+            0|1|2|3|292|293|294) echo -e "g$cod $val" | ./data/lib/ftp/uavpal/bin/netcat-arm -u ${ipESP} 8888 -w1&;;
             esac
         fi 	
 	done
